dedd66296842f1bf14ee70c9db29415a671d1a0c
Marcos Pilaze <marcospilaze2@gmail.com>
2022-04-04 01:28:00 -0300

    DesafioJS

---
+++ desfio.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+// let cantidad = prompt("Cuantos Articulos desea Cargar?")
+// for (i = 0; i < cantidad; i++){
+
+
+// let productos = prompt("Ingrese un Producto");
+
+// let precio = parseInt(prompt("Ingrese Precio del producto"));
+// descuento = precio * 0.25 / 100;
+
+
+
+    
+    
+//     if( precio < 500){
+//         descuento = precio - descuento;
+//           alert(`Tiene descuento ${descuento}` );
+
+//     }else{
+//         alert("Este producto no tiene descuento");
+        
+//     }
+  
+
+// }
+// alert("Gracias Por su compra");
+
+
+
+
+
+let cancion= prompt("Ingrese su cancion").toLocaleLowerCase();
+
+while(cancion != "no"){
+
+  alert (`Acabamos de oir ${cancion}`);
+
+alert("En caso de no querer escuchar mas escriba no");
+
+cancion= prompt(" Ingrese su cancion" );
+
+ }alert("Muchcas gracias, espero que les haya gustado");
+
+
+
+
+
